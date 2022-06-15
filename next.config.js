3d99033b44deea153eb49aa2200e1c4fc0abbb33
Ryan Park <rpark@Ryans-MBP-5.lan>
2022-06-15 05:25:41 -1000

    Magic Link Integrated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['emojipedia-us.s3.dualstack.us-west-1.amazonaws.com'],
+  },
 }
 
 module.exports = nextConfig
